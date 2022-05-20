8a3f589c49b7c712690a25a1f17f86412536f1b7
Gaddam Lakshmi Prasanna <99520627+lakshmiprasannaG@users.noreply.github.com>
2022-05-20 17:14:47 +0530

    Removed readFile function.

--- head.js
+++ head.js
@@ -3,14 +3,7 @@ const { head } = require('./src/headLib.js');
 
 const main = content => head(content);
 
-const readFile = function (fileName) {
-  try {
-    return fs.readFileSync(fileName, 'utf8');
-  } catch (error) {
-    return 'usage: head [-n lines | -c bytes] [file ...]';
-  }
-};
+const headUsage = 'usage: head [-n lines | -c bytes] [file ...]';
 
-const fileContent = readFile(process.argv[2]);
-
-console.log(fileContent ? main(fileContent) : fileContent);
+const filePath = process.argv[2];
+console.log(filePath ? main(fs.readFileSync(filePath, 'utf8')) : headUsage);
